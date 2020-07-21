8bdd73daa046567255bc065350c7c9c46c533863
Everton Sales <46347115+tomsales92@users.noreply.github.com>
2020-07-20 23:35:48 -0300

    method create product

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
@@ -43,7 +44,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatListModule,
     MatCardModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule
 
     
   ],
--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,12 +1,16 @@
+import { Product } from './product.model';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from  '@angular/material/snack-bar';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  baseUrl = "http://localhost:3001/products";
 
-  constructor(private snackBar: MatSnackBar) { }
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
   showMessage(msg:string): void {
    this.snackBar.open(msg, 'x', {
@@ -15,4 +19,10 @@ export class ProductService {
      verticalPosition: "top"
    })
   }
+
+  create(product: Product):Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product);
+  }
+
+
 }

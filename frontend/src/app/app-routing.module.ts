43e232908b6815d96d5ddfec0e128c0aadf7db0f
Everton Sales <everton.goncalves92@gmail.com>
2020-07-24 22:57:26 -0300

    Configuration method delete

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import {HomeComponent} from './views/home/home.component';
 import {ProductCrudComponent} from './views/product-crud/product-crud.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 
 const routes: Routes = [
@@ -24,6 +25,10 @@ const routes: Routes = [
 {
   path: 'products/update/:id',
   component: ProductUpdateComponent
+},
+{
+  path: 'products/delete/:id',
+  component: ProductDeleteComponent
 }
 ];
 
